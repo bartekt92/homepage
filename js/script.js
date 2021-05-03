12cd95ffdae3ef52163052681202cb587e616a2d
Barłomiej Tabero <bartekt92@gmail.com>
2021-05-03 13:47:38 +0200

    script functions refacotried

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-console.log("czesc");
-let button = document.querySelector(".button");
-let invisible = document.querySelector(".section__js-invisible");
-let showText = "rozwiń";
-let hiddenText = "zwiń";
-button.innerText = showText;
-button.addEventListener("click", () => {
+const welcome = () => {
+    console.log("Witajcie programiści!");
+}
+welcome();
+const invisible = document.querySelector(".section__js-invisible");
+const expandOnClick = () => {
     invisible.classList.toggle("section__js-invisible");
-    button.innerText = button.innerText === hiddenText ? showText : hiddenText;
-});
+    button.innerText = button.innerText === "zwiń" ? "rozwiń" : "zwiń";
+};
+
+const button = document.querySelector(".button");
+button.innerText = "rozwiń";
+
+button.addEventListener("click", expandOnClick);
