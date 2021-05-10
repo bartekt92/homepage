9709ec18fe35533e8df3280ba951e77b5c1814e0
Barłomiej Tabero <bartekt92@gmail.com>
2021-05-10 18:11:32 +0200

    script refactored

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const welcome = () => {
-    console.log("Witajcie programiści!");
-}
-const expandOnClick = () => {
-    const extraParagraphElement = document.querySelector(".js-invisible");
-    extraParagraphElement.classList.toggle("section__invisible");
-    button.innerText = button.innerText === "zwiń" ? "rozwiń" : "zwiń";
-};
-const init = () =>{
-    welcome();
+{
     const button = document.querySelector(".button");
     button.innerText = "rozwiń";
-    button.addEventListener("click", expandOnClick);
+    const welcome = () => {
+        console.log("Witajcie programiści!");
+    }
+    const expandOnClick = () => {
+        const extraParagraphElement = document.querySelector(".js-invisible");
+        extraParagraphElement.classList.toggle("section__invisible");
+        button.innerText = button.innerText === "zwiń" ? "rozwiń" : "zwiń";
+    };
+    const init = () => {
+        welcome();
+        button.addEventListener("click", expandOnClick);
+    }
+    init();
 }
-init();
